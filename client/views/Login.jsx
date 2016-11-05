d24e307650a4c6b0ef28b5752d9db4e8809af4e0
Pierce Dhaese <pdhaese@wustl.edu.com>
2016-11-05 17:12:05 -0500

    Register and login pages

--- client/views/Login.jsx
+++ client/views/Login.jsx
@@ -1,5 +1,36 @@
 Login = React.createClass({
+  getDefaultProps() {
+    let registerMsg = "Don't have an account?";
+    return {
+      registerLink: <p>{registerMsg} <a href="/register">Register</a></p>
+    };
+  },
+  loginWithPassword(e) {
+    e.preventDefault();
+    const email = $('#email').val(),
+          password = $('#password').val().trim()
+    ;
+
+    Meteor.loginWithPassword(email, password, function(error) {
+      if (error) {
+        console.log("There was an error:" + error.reason);
+      } else {
+        FlowRouter.go('/');
+      }
+    });
+  },
   render() {
-    return <div> Login view</div>;
-  }
+        return (
+            <div className="row">
+        <div className="col-md-6 col-md-offset-3">
+          <h1>Login</h1>
+            <EmailPasswordForm
+              submitBtnLabel="Login"
+              submitAction={this.loginWithPassword}
+            />
+                  {this.props.registerLink}
+        </div>
+      </div>
+        )
+    }
 });
